029ca47dec6ef6ccdcda882105b1e356f5e68c67
AipBorges <mpbds@uic.es>
2021-10-15 22:26:04 +0200

    added page component

--- js/Page/Page.js
+++ js/Page/Page.js
@@ -1,15 +1,38 @@
-class Component {
+import Component from "../Component/Component.js";
+
+class Page extends Component {
   element;
   parentElement;
 
-  constructor(className, htmlTag) {
-    this.element = document.createElement(htmlTag);
-    this.element.className = className;
-    this.parentElement = document.querySelector("body");
-    this.parentElement.append(this.element);
+  constructor() {
+    super(".main", ".main-page", "div");
+
+    this.generateHtml();
+  }
+
+  generateHtml() {
+    const htmlText = `    <main class="main-pokeside">
+      <header>
+        <nav class="main-bar">
+          <div class="main-bar__logo">
+            <img
+              class="main-bar__picture"
+              src="./pokemon-logo.svg"
+              alt="Poke logo"
+            />
+          </div>
+          <h1 class="main-bar__title">Hello_</h1>
+          <label for="main-bar__-input">poke-search</label>
+          <input type="text" id="main-bar__poke-input" />
+          <button class="main-bar__button">Catch 'em!</button>
+        </nav>
+      </header>
+
+      <section class="card-section">
+
+      </section>
+    </main>`;
   }
 }
 
-export default Component;
-
-
+export default Page;
