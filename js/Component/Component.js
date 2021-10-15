2f1bdbb52c8affca25bef4405b03ffaafde1c993
AipBorges <mpbds@uic.es>
2021-10-15 22:26:27 +0200

    created services class mockup

--- js/Component/Component.js
+++ js/Component/Component.js
@@ -5,12 +5,9 @@ class Component {
   constructor(parentElement, className, htmlTag) {
     this.element = document.createElement(htmlTag);
     this.element.className = className;
-    this.parentElement = parentElement;
+    this.parentElement = document.querySelector(parentElement);
     this.parentElement.append(this.element);
   }
 }
 
 export default Component;
-
-
-this.parentElement = document.querySelector("body");
---
+++ js/Services/Services.js
@@ -0,0 +1,45 @@
+class Services {
+  limit = 12;
+  offset = 0;
+  url = `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`;
+
+  async getSeries() {
+    const response = await fetch(this.url);
+    const pokemons = await response.json();
+    return pokemons;
+  }
+  /* 
+  async createSerie(serie) {
+    const response = await fetch(${this.urlAPI}, {
+      method: "POST",
+      body: JSON.stringify(serie),
+      header: {
+        "Content-Type": "application/json",
+      },
+    });
+    const newSerie = await response.json();
+    return newSerie;
+  }
+
+  async updateSerie(id, serie) {
+    fetch(${this.urlAPI}${id}, {
+      method: "PUT",
+      body: JSON.stringify(serie),
+      header: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  async deleteSerie(id) {
+    const response = await fetch(${this.urlAPI}${id}, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      return true;
+    }
+    throw new Error("Can't delete it");
+  } */
+}
+
+export default Services;
