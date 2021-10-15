73aa4ac076bca37e3edd34c63d1f355abc36085a
AipBorges <mpbds@uic.es>
2021-10-15 18:04:46 +0200

    initial files added

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "lines-between-class-members": "off",
+    "class-methods-use this": "off",
+    "import/extensions": "off",
+  },
+  jest: {
+    testEnvironment: "jsdom",
+  },
+  parser: "@babel/eslint-parser",
+};
